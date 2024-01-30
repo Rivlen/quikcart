34d09683a269244484a589545fd8ac26b38b8a13
Rivlen <x.stawecki@gmail.com>
2024-01-30 09:18:21 +0100

    Bootstrap fix

--- quikcart/urls.py
+++ quikcart/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main.views import HomePageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='index'),
 ]
