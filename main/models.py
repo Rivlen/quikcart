687e5851a9e787203cd02c53efce540f36346e0d
Rivlen <x.stawecki@gmail.com>
2024-02-01 11:38:38 +0100

    Add feature, upgrade product models

--- main/models.py
+++ main/models.py
@@ -13,11 +13,22 @@ class Product(models.Model):
     categories = models.ManyToManyField(Category, related_name='products')
     name = models.CharField(max_length=200)
     description = models.TextField()
+    long_description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    previous_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Feature(models.Model):
+    product = models.ForeignKey(Product, related_name='features', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
