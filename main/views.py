74ab537cc76f66fe3337eabce9457cb5c2a1dd13
Rivlen <x.stawecki@gmail.com>
2024-02-01 11:40:25 +0100

    Update product details

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
+from main.models import Product, Category
 
 
 class HomePageView(View):
@@ -20,3 +21,12 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'shop-single.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the features for the product
+        context['features'] = self.object.features.all()
+        # Additionally, add in a QuerySet of all the categories for the product
+        context['categories'] = self.object.categories.all()
+        return context
