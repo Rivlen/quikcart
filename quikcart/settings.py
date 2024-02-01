020a4521cf82c6f09438ccdcc23bb2ce79eaae53
Rivlen <x.stawecki@gmail.com>
2024-02-01 13:41:57 +0100

    Add userbase

--- quikcart/settings.py
+++ quikcart/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "main.apps.MainConfig"
+    "main.apps.MainConfig",
+    "userbase.apps.UserbaseConfig"
 ]
 
 MIDDLEWARE = [
