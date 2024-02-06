1f4c567ef3f13bbfc16bf34cefed037d1379f3fd
Rivlen <x.stawecki@gmail.com>
2024-02-06 17:14:26 +0100

    Fix logout

--- userbase/urls.py
+++ userbase/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from .views import MemberSignUpView, UserProfileView, PurchaseHistoryView, UserProductsView
 from django.contrib.auth import views as auth_views
@@ -5,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', MemberSignUpView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('purchases/', PurchaseHistoryView.as_view(), name='purchase-history'),
     path('products/', UserProductsView.as_view(), name='user-products'),
